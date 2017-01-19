773857adc2386d36069819404c3751e029004ffe
zk475811 <zk475811@ohio.edu>
2017-01-19 18:29:01 -0500

    Update select_file_main.cpp

--- select_file_main.cpp
+++ select_file_main.cpp
@@ -27,15 +27,15 @@ int WINAPI WInMain(HINSTANCE hInstance, HINSTANCE, hPrevInstance, LPSTR lpCmdLin
   wcex.style = CS_HREDRAW | CS_VREDRAW;
   wcex.lpfnWndProc = WndProc;
   wcex.cbClsExtra = 0;
-	wcex.cbWndExtra = 0;
-	wcex.hInstance = hInstance;
-	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
-	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+  wcex.cbWndExtra = 0;
+  wcex.hInstance = hInstance;
+  wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+  wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
   //change background color
-	wcex.hbrBackground = (HBRUSH)(COLOR_BACKGROUND + 1);
-	wcex.lpszMenuName = NULL;
-	wcex.lpszClassName = szWindowClass;
-	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+  wcex.hbrBackground = (HBRUSH)(COLOR_BACKGROUND + 1);
+  wcex.lpszMenuName = NULL;
+  wcex.lpszClassName = szWindowClass;
+  wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
   
   //test if class registered 
   if (!RegisterClassEx(&wcex))
